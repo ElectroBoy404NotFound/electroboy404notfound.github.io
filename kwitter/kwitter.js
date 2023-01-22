f69197931c09d54169a51bfdbc68a7870c5dbf0f
ElectroCoder <electroboy404notfound@gmail.com>
2023-01-22 08:02:39 +0000

    SHA256

--- kwitter/kwitter.js
+++ kwitter/kwitter.js
@@ -10,7 +10,7 @@ var firebaseConfig = {
 firebase.initializeApp(firebaseConfig);
 
 async function sha512(str) {
-    const buf = await crypto.subtle.digest("SHA-512", new TextEncoder("utf-8").encode(str));
+    const buf = await crypto.subtle.digest("SHA-256", new TextEncoder("utf-8").encode(str));
     return Array.prototype.map.call(new Uint8Array(buf), x=>(('00'+x.toString(16)).slice(-2))).join('');
   }
 function logUserIn() {
--- kwitter/kwitter_page.js
+++ kwitter/kwitter_page.js
@@ -13,7 +13,7 @@ var room_name = localStorage.getItem("room");
 var username = localStorage.getItem("user_name");
 var lastmsgHASH = "";
 async function sha512(str) {
-      const buf = await crypto.subtle.digest("SHA-512", new TextEncoder("utf-8").encode(str));
+      const buf = await crypto.subtle.digest("SHA-256", new TextEncoder("utf-8").encode(str));
       return Array.prototype.map.call(new Uint8Array(buf), x=>(('00'+x.toString(16)).slice(-2))).join('');
     }
 function getData() { 
